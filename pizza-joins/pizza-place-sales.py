88fa74d740aeef670cbf635bfad5126c2f8c367c
Toludoyin <toludoyinshopein@gmail.com>
2023-05-21 01:16:43 +0100

    modify pizza-sales

--- pizza-joins/pizza-place-sales.py
+++ pizza-joins/pizza-place-sales.py
@@ -3,16 +3,16 @@ import pandas as pd
 
 
 def read_data(data):
-    df = pd.read_csv('pizza-sales-data/' + data)
+    df = pd.read_csv('pizza-sales-data/' + data, encoding='latin1')
     return df
 
-orders = read_data('orders.csv')
+order = read_data('orders.csv')
 order_detail = read_data('order_details.csv')
 pizza = read_data('pizzas.csv')
 pizza_type = read_data('pizza_types.csv')
 
-# merge_csv('orders.csv')
-# data =DATA_PATH.joinpath('orders.csv').merge(DATA_PATH.joinpath('order_details.csv'), on= "order_id", how= "left")
-print(orders)
+orders_detail_merge = order.merge(order_detail, on = "order_id", how = "left")
+pizza_merge = orders_detail_merge.merge(pizza, on = "pizza_id", how = "left")
+pizza_type_merge = pizza_merge .merge(pizza_type, on = "pizza_type_id", how = "left")
 
-
+pizza_type_merge.to_csv('pizza-sales.csv', index=False)
